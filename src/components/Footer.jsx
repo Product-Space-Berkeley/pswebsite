6824ec95a2b3ccf8729a6a67f2f24014f22c3a63
carlychvn <78331148+carlychvn@users.noreply.github.com>
2025-01-16 15:19:16 -0800

    home, navbar, footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,19 +2,20 @@
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import HomeLogo from './images/miscicons/psHomeLogo.png'
+import psFooterLogo from '/Users/carly/Desktop/psweb2/pswebsite/src/components/images/miscicons/FooterLogo.png'
 
 const handleLinkClick = () => {
     window.scrollTo(0, 0);
   };
 
 
-function Footer() {
+  function Footer() {
     return (
       <Container>
         <ItemContainer>
-        <Item >
+        <Item>
             <Link to="/Home" onClick={handleLinkClick}>
-                <HomeIcon src={HomeLogo} alt='ImageNotLoading'/>
+                <Image src={psFooterLogo} alt="Footer Logo" />
             </Link>
             <Text> 
                 We are a student group acting 
@@ -47,91 +48,101 @@ function Footer() {
   
   export default Footer;
 
-const Container = styled.div`
-    width: 100%;
-    height: auto;
-    background-image: radial-gradient(circle at 1px 1px, #bdbab9 2px, transparent 0);
-    background-size: 30px 30px;
-    
+  const Container = styled.div`
+  width: 100%;
+  height: auto;
+  background-image: linear-gradient(to right, rgba(117, 61, 146, 1), rgba(163, 100, 165, 1), rgba(208, 144, 157, 1));
+  
 
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 
-    // border: solid black;
+  // border: solid black;
 
-    @media only screen and (max-width: 400px) {
-        display: none;
-    }
+  @media only screen and (max-width: 400px) {
+      display: none;
+  }
 
 `
 const ItemContainer = styled.div`
-    padding-top: 50px;
-    padding-left: 50px;
-    padding-bottom: 50px;
-    padding-right: 50px;
-    margin-right: 50px;
-    gap: 50px;
+  padding-top: 50px;
+  padding-left: 50px;
+  padding-bottom: 50px;
+  padding-right: 50px;
+  margin-right: 50px;
+  gap: 50px;
 
-    width: 80%;
-    // border: solid yellow;
-
-
-    display: flex; 
-    align-items: center; 
-    flex-direction: row;
-    flex-wrap: wrap;
+  width: 80%;
+  // border: solid yellow;
 
 
-    @media only screen and (max-width: 400px) {
-        gap: 10px;
-        padding-bottom: 0px;
-    }
+  display: flex; 
+  align-items: center; 
+  flex-direction: row;
+  flex-wrap: wrap;
+
+
+  @media only screen and (max-width: 400px) {
+      gap: 10px;
+      padding-bottom: 0px;
+  }
 `
 
 const Item = styled.div`
-    height: 300px; 
-    flex-grow: 1;
-    max-width: 200px;
-    min-width: 150px;
-    padding-left: 30px;
-    // border: solid black;
+  height: 300px; 
+  flex-grow: 1;
+  max-width: 200px;
+  min-width: 150px;
+  padding-left: 30px;
+  // border: solid black;
 
-    
-    display: flex; 
-    flex-direction: column;
+  
+  display: flex; 
+  flex-direction: column;
 
-    @media only screen and (max-width: 400px) {
-        flex-wrap: wrap;
-        // border: solid yellow;
-    }
-    
+  @media only screen and (max-width: 400px) {
+      flex-wrap: wrap;
+      // border: solid yellow;
+  }
+  
 
-    // border: solid black;
+  // border: solid black;
 
 `
+
+const Image = styled.img`
+  height: 70px;
+  padding-bottom: 10px;
+  width: auto;
+  margin-left: -20px;
+  margin-top: -20px;
+`
+
 const HomeIcon = styled.img`
-    height: 60px;
-    padding-bottom: 10px;
+  height: 60px;
+  padding-bottom: 10px;
 `
 
 const Header = styled.div`
-    font-size: 24px;
-    margin-bottom: 50px;
+  font-size: 24px;
+  margin-bottom: 25px;
+  color: white;
 `
 
 const SubHeader = styled(Link)`
-    color: black;
-    font-size: 20px;
-    text-decoration: none;
-    padding-bottom: 20px;
+  color: black;
+  font-size: 20px;
+  text-decoration: none;
+  padding-bottom: 20px;
+  color: white;
 `
 
 
 
 const Text = styled.div`
-    font-size: 16px;
-    line-height: 2;
+  font-size: 16px;
+  line-height: 2;
+  color: white;
 
-`   
-
+` 
 
--- src/components/HomePage/ForCompanies.jsx
+++ src/components/HomePage/ForCompanies.jsx
@@ -11,94 +11,120 @@ const handleLinkClick = () => {
 
 function ForCompanies() {
     return (
-        <Container> 
+      <Container>
         <Box>
-            <Title> FOR COMPANIES</Title>
-            <Description> 
-            The best undergraduate team for your products
-            </Description>
-            <Description2>
-            Product Space is uniquely positioned to assist
-             companies as the first and largest product management
-              organization at UC Berkeley. All client projects are
-               handled by a project manager, 4 to 6 product associates,
-                and an advisor.
-            </Description2>
-            <Button to="/Companies" onClick={handleLinkClick}> Learn More </Button>
+          <Title>Client Projects</Title>
+          <Description2>
+            Product Space is uniquely positioned to assist companies as the first
+            and largest product management organization at UC Berkeley. All client
+            projects are handled by a project manager, 4 to 6 product associates,
+            and an advisor.
+          </Description2>
+          <Button to="/Companies" onClick={handleLinkClick}>
+            Learn More
+          </Button>
         </Box>
         <PictureBox src={sp23Board} />
-    </Container>
-    )
-};
-
+      </Container>
+    );
+}
+  
 export default ForCompanies;
-const Container = styled.div`
-    display: flex; 
-    justify-content: space-around;
-    padding-bottom: 70px;
-    padding-top: 70px;
 
-    // border: solid black;
-`
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding-bottom: 70px;
+  padding-top: 70px;
+  max-width: 80%;
+  margin: 0 auto;
+  height: auto;
+  width: 100%;
+  flex-wrap: wrap; 
+
+  @media only screen and (max-width: 900px) {
+    flex-direction: column; 
+    padding: 20px;
+  }
+`;
 
 const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 500px;
+  height: auto;
+  padding-left: 20px;
+
+  @media only screen and (max-width: 450px) {
+    width: 80%; 
     display: flex;
-    // flex: 1;
-    width: 500px;
-    flex-direction: column;
-
-    // border: solid blue;
-
-    @media only screen and (max-width: 450px) {
-        width: 70%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        text-align: center;
-        // border: solid yellow;
-    }
-
-`
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    margin-bottom: 20px; 
+  }
+`;
 
 const Title = styled.h3`
-    color: #EB5B8D;
-    font-size: 24px;
-`
+  color: #592e73;
+  font-size: 56px;
+  margin-bottom: 10px;
+
+  @media only screen and (max-width: 450px) {
+    font-size: 36px; 
+  }
+`;
 
 const Description = styled.h1`
-    color: black;
-    font-size: 34px;
-    font-weight: 450;
-`
+  color: black;
+  font-size: 34px;
+  font-weight: 450;
+  margin-bottom: 10px;
+
+  @media only screen and (max-width: 450px) {
+    font-size: 24px; 
+  }
+`;
 
 const Description2 = styled.h2`
-    color: #656565; 
-    font-size: 20;
-    font-weight: normal;
-`
+  color: #656565;
+  font-size: 20px;
+  font-weight: normal;
+  margin-bottom: 30px;
+
+  @media only screen and (max-width: 450px) {
+    font-size: 18px; 
+    text-align: center; 
+  }
+`;
 
 const Button = styled(Link)`
-    width: 100px;
-    height: 40px;
-    font-size: 16px;
-    font-weight: 800;
-    background: transparent; 
-    border: none;
-    border-bottom: 3px solid #EB5594;
-    text-decoration: none;
-    color: black;
-`
+  width: 100px;
+  height: 30px;
+  font-size: 16px;
+  font-weight: 800;
+  background: transparent;
+  border: none;
+  border-bottom: 3px solid #eb5594;
+  text-decoration: none;
+  color: black;
+  align-text: center;
+`;
 
 const PictureBox = styled.img`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 700px;
-    height: 480px;
-    margin-left: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 700px;
+  height: 480px;
+  border-radius: 30px;
+  margin-right: 20px;
 
-    @media only screen and (max-width: 900px) {
-        display: none;
-    }
-
-`
+  @media only screen and (max-width: 900px) {
+    width: 100%; 
+    height: auto; 
+    margin-right: 0; 
+    margin-top: 20px; 
+  }
+`;
